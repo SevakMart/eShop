f1f0751f4dc4311bbbf214beb7a8d6c59cbe363e
forjava <forjava>
2016-11-28 16:31:07 +0400

    add cart dao and dao interfeys

---
+++ src/main/java/dataaccess/dao/ICartDAO.java
@@ -0,0 +1,9 @@
+package dataaccess.dao;
+
+import model.Cart;
+
+/**
+ * Created by forjava on 11/28/2016.
+ */
+public interface ICartDAO extends GenericDAO<Cart> {
+}
---
+++ src/main/java/dataaccess/dao/impl/CartDAOImpl.java
@@ -0,0 +1,36 @@
+package dataaccess.dao.impl;
+
+import dataaccess.dao.ICartDAO;
+import model.Cart;
+
+import java.util.List;
+
+/**
+ * Created by forjava on 11/28/2016.
+ */
+public class CartDAOImpl implements ICartDAO {
+    @Override
+    public void create(Cart cart) {
+
+    }
+
+    @Override
+    public Cart getEntityByID(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Cart> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Cart entity) {
+
+    }
+
+    @Override
+    public void delete(Cart entity) {
+
+    }
+}
